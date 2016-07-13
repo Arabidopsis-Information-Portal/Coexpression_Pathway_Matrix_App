2360e2d935a9d1f3854617eeb68c0e9d757e6ba4
eriksf <eferlanti@tacc.utexas.edu>
2016-07-13 12:16:33 -0500

    now using new app spec with icon, tags, and screenshots

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,25 +11,13 @@
     window.addEventListener('Agave::ready', function() {
 
 	var Agave = window.Agave;
-
-	// Data for building example gene bar graph
-	var default_gene_json =
-	    {'result': [
-		{'relationships': [{'direction': 'undirected', 'type': 'coexpression', 'scores': [{'correlation_coefficient': 0.8089}]}], 'related_entity': 'AT1G08260', 'class': 'locus_relationship', 'locus': 'AT5G63960'},
-		{'relationships': [{'direction': 'undirected', 'type': 'coexpression', 'scores': [{'correlation_coefficient': 0.8035}]}], 'related_entity': 'AT4G24270', 'class': 'locus_relationship', 'locus': 'AT5G63960'},
-		{'relationships': [{'direction': 'undirected', 'type': 'coexpression', 'scores': [{'correlation_coefficient': 0.7967}]}], 'related_entity': 'AT5G67100', 'class': 'locus_relationship', 'locus': 'AT5G63960'},
-		{'relationships': [{'direction': 'undirected', 'type': 'coexpression', 'scores': [{'correlation_coefficient': 0.7781}]}], 'related_entity': 'AT5G18620', 'class': 'locus_relationship', 'locus': 'AT5G63960'},
-		{'relationships': [{'direction': 'undirected', 'type': 'coexpression', 'scores': [{'correlation_coefficient': 0.8025}]}], 'related_entity': 'AT1G06670', 'class': 'locus_relationship', 'locus': 'AT5G63960'},
-		{'relationships': [{'direction': 'undirected', 'type': 'coexpression', 'scores': [{'correlation_coefficient': 0.7801}]}], 'related_entity': 'AT5G49430', 'class': 'locus_relationship', 'locus': 'AT5G63960'},
-		{'relationships': [{'direction': 'undirected', 'type': 'coexpression', 'scores': [{'correlation_coefficient': 0.7524}]}], 'related_entity': 'AT4G02070', 'class': 'locus_relationship', 'locus': 'AT5G63960'},
-		{'relationships': [{'direction': 'undirected', 'type': 'coexpression', 'scores': [{'correlation_coefficient': 0.7432}]}], 'related_entity': 'AT1G30590', 'class': 'locus_relationship', 'locus': 'AT5G63960'},
-		{'relationships': [{'direction': 'undirected', 'type': 'coexpression', 'scores': [{'correlation_coefficient': 0.7577}]}], 'related_entity': 'AT3G42660', 'class': 'locus_relationship', 'locus': 'AT5G63960'},
-		{'relationships': [{'direction': 'undirected', 'type': 'coexpression', 'scores': [{'correlation_coefficient': 0.7895}]}], 'related_entity': 'AT4G19610', 'class': 'locus_relationship', 'locus': 'AT5G63960'},
-		{'relationships': [{'direction': 'undirected', 'type': 'coexpression', 'scores': [{'correlation_coefficient': 0.7472}]}], 'related_entity': 'AT1G63640', 'class': 'locus_relationship', 'locus': 'AT5G63960'},
-		{'relationships': [{'direction': 'undirected', 'type': 'coexpression', 'scores': [{'correlation_coefficient': 0.794}]}], 'related_entity': 'AT3G52140', 'class': 'locus_relationship', 'locus': 'AT5G63960'}
-	    ],
-	     'metadata': {'time_in_main': null},
-	     'status': 'success'};
+	var nodes, n, orders, timeout, type;
+	var matrix = [];
+	var margin = {top: 60, right: 20, bottom: 10, left: 60}, width = 800, height = 800;
+	var params = [];
+	var x = d3.scale.ordinal().rangeBands([0, width]),
+	    z = d3.scale.linear().domain([0, 4]).clamp(true),
+	    c = d3.scale.category10().domain(d3.range(10));
 
 	/* - - - - - - - - - -  */
 	/* Start app log */
@@ -112,6 +100,39 @@
 	}
 
 	/* - - - - - - - - - -  - - -*/
+	/* Setup mouseover functions-*/
+	/* - - - - - - - - - -  - - -*/
+	function mouseover(p) {
+	    d3.selectAll('.row text').classed('active', function(d, i) { return i === p.y; });
+	    d3.selectAll('.column text').classed('active', function(d, i) { return i === p.x; });
+	}
+
+	/* - - - - - - - - - -  - - -*/
+	/* Setup mouseout functions -*/
+	/* - - - - - - - - - -  - - -*/
+	function mouseout() {
+	    d3.selectAll('text').classed('active', false);
+	}
+
+	/* - - - - - - - - - -  - - -*/
+	/* Define rowA dimensions  --*/
+	/* - - - - - - - - - -  - - -*/
+	function rowA(row) {
+        /*jshint validthis: true */
+	    d3.select(this).selectAll('.cellA')
+		.data(row.filter(function(d) { return d.z; }))
+		.enter().append('rect')
+		.attr('class', '.cellA')
+		.attr('x', function(d) { return x(d.x); })
+		.attr('width', x.rangeBand())
+		.attr('height', x.rangeBand())
+		.style('fill-opacity', function(d) { return z(d.z); })
+		.style('fill', function(d) { return nodes[d.x].group === nodes[d.y].group ? c(nodes[d.x].group) : null; })
+		.on('mouseover', mouseover)
+		.on('mouseout', mouseout);
+	}
+
+	/* - - - - - - - - - -  - - -*/
 	/* Setup the rows A - - - - -*/
 	/* - - - - - - - - - -  - - -*/
 	function setRowA(container){
@@ -125,6 +146,24 @@
 	}
 
 	/* - - - - - - - - - -  - - -*/
+	/* Define rowB dimensions  --*/
+	/* - - - - - - - - - -  - - -*/
+	function rowB(row) {
+        /*jshint validthis: true */
+	    d3.select(this).selectAll('.cellB')
+		.data(row.filter(function(d) { return d.z; }))
+		.enter().append('rect')
+		.attr('class', '.cellB')
+		.attr('x', function(d) { return x(d.x); })
+		.attr('width', x.rangeBand())
+		.attr('height', x.rangeBand())
+		.style('fill-opacity', function(d) { return z(d.z); })
+		.style('fill', function(d) { return nodes[d.x].group === nodes[d.y].group ? c(nodes[d.x].group) : null; })
+		.on('mouseover', mouseover)
+		.on('mouseout', mouseout);
+	}
+
+	/* - - - - - - - - - -  - - -*/
 	/* Setup the rows B - - - - -*/
 	/* - - - - - - - - - -  - - -*/
 	function setRowB(container){
@@ -135,6 +174,24 @@
 		.attr('transform', function(d, i) {  return 'translate(0,' + x(i) + ')'; })
 		.each(rowB);
 	    return line;
+	}
+
+	/* - - - - - - - - - -  - - -*/
+	/* Define rowC dimensions  --*/
+	/* - - - - - - - - - -  - - -*/
+	function rowC(row) {
+        /*jshint validthis: true */
+	    d3.select(this).selectAll('.cellC')
+		.data(row.filter(function(d) { return d.z; }))
+		.enter().append('rect')
+		.attr('class', '.cellC')
+		.attr('x', function(d) { return x(d.x); })
+		.attr('width', x.rangeBand())
+		.attr('height', x.rangeBand())
+		.style('fill-opacity', function(d) { return z(d.z); })
+		.style('fill', function(d) { return nodes[d.x].group === nodes[d.y].group ? c(nodes[d.x].group) : null; })
+		.on('mouseover', mouseover)
+		.on('mouseout', mouseout);
 	}
 
 	/* - - - - - - - - - -  - - -*/
@@ -164,85 +221,6 @@
 	}
 
 	/* - - - - - - - - - -  - - -*/
-	/* Define rowA dimensions  --*/
-	/* - - - - - - - - - -  - - -*/
-	function rowA(row) {
-
-	    var cell = d3.select(this).selectAll('.cellA')
-		.data(row.filter(function(d) { return d.z; }))
-		.enter().append('rect')
-		.attr('class', '.cellA')
-		.attr('x', function(d) { return x(d.x); })
-		.attr('width', x.rangeBand())
-		.attr('height', x.rangeBand())
-		.style('fill-opacity', function(d) { return z(d.z); })
-		.style('fill', function(d) { return nodes[d.x].group === nodes[d.y].group ? c(nodes[d.x].group) : null; })
-		.on('mouseover', mouseover)
-		.on('mouseout', mouseout);
-	}
-
-	/* - - - - - - - - - -  - - -*/
-	/* Define rowB dimensions  --*/
-	/* - - - - - - - - - -  - - -*/
-	function rowB(row) {
-
-	    var cell = d3.select(this).selectAll('.cellB')
-		.data(row.filter(function(d) { return d.z; }))
-		.enter().append('rect')
-		.attr('class', '.cellB')
-		.attr('x', function(d) { return x(d.x); })
-		.attr('width', x.rangeBand())
-		.attr('height', x.rangeBand())
-		.style('fill-opacity', function(d) { return z(d.z); })
-		.style('fill', function(d) { return nodes[d.x].group === nodes[d.y].group ? c(nodes[d.x].group) : null; })
-		.on('mouseover', mouseover)
-		.on('mouseout', mouseout);
-	}
-
-	/* - - - - - - - - - -  - - -*/
-	/* Define rowC dimensions  --*/
-	/* - - - - - - - - - -  - - -*/
-	function rowC(row) {
-
-	    var cell = d3.select(this).selectAll('.cellC')
-		.data(row.filter(function(d) { return d.z; }))
-		.enter().append('rect')
-		.attr('class', '.cellC')
-		.attr('x', function(d) { return x(d.x); })
-		.attr('width', x.rangeBand())
-		.attr('height', x.rangeBand())
-		.style('fill-opacity', function(d) { return z(d.z); })
-		.style('fill', function(d) { return nodes[d.x].group === nodes[d.y].group ? c(nodes[d.x].group) : null; })
-		.on('mouseover', mouseover)
-		.on('mouseout', mouseout);
-	}
-
-	/* - - - - - - - - - -  - - -*/
-	/* Setup mouseover functions-*/
-	/* - - - - - - - - - -  - - -*/
-	function mouseover(p) {
-	    d3.selectAll('.row text').classed('active', function(d, i) { return i === p.y; });
-	    d3.selectAll('.column text').classed('active', function(d, i) { return i === p.x; });
-	}
-
-	/* - - - - - - - - - -  - - -*/
-	/* Setup mouseout functions -*/
-	/* - - - - - - - - - -  - - -*/
-	function mouseout() {
-	    d3.selectAll('text').classed('active', false);
-	}
-
-	/* - - - - - - - - - -  - - -*/
-	/* Set the order wrapper- - -*/
-	/* - - - - - - - - - -  - - -*/
-	function setOrder(o,container){
-	    d3.select(o).on('change', function() {
-		clearTimeout(timeout);
-		order(this.value,container);
-	    });
-	}
-
-	/* - - - - - - - - - -  - - -*/
 	/* Create order - - - - - - -*/
 	/* - - - - - - - - - -  - - -*/
 	function order(value,container,cId) {
@@ -264,191 +242,22 @@
 	}
 
 	/* - - - - - - - - - -  - - -*/
+	/* Set the order wrapper- - -*/
+	/* - - - - - - - - - -  - - -*/
+	function setOrder(o,container){
+	    d3.select(o).on('change', function() {
+		clearTimeout(timeout);
+		order(this.value,container);
+	    });
+	}
+
+	/* - - - - - - - - - -  - - -*/
 	/* Setup timeout  - - - - - -*/
 	/* - - - - - - - - - -  - - -*/
 	function setTimeout(o,container,cId){
 	    order('group',container,cId);
 	    d3.select(o).property('selectedIndex', 2).node().focus();
 	}
-
-
-	/* - - - - - - - - - -  - -  - - - - - --*/
-	/* Generate a gene matrix wrapper  - - - - - -*/
-	/* - - - - - - - - - -  - - - - - - - - -*/
-	function createGeneChartWrapper(json){
-
-	    // The search is done, hide the waiting bar, clear error message container
-	    $('#wait_region4').empty();
-	    $('#errorD').empty();
-	    $('#matrix_container_d').empty();
-
-	    // Verify the API query was successful
-	    if ( ! json || json.status !== 'success') {
-		$('#errorD', appContext).html('<div class="alert alert-danger">Error with retrieval!</div>');
-		return;
-            }
-
-	    // Begin parsing json
-	    var jobj = json.obj || json;
-
-	    // Initialize link objects
-	    var geneObj = [];
-	    var dataObj = [];
-
-	    jobj.result.forEach(function(object){
-		var linkingGene = object.related_entity;
-
-		// Sometimes the webservice returns incomplete data
-		if(typeof object.relationships !== 'undefined'){
-		    // Each linking gene
-		    object.relationships.forEach(function(relObj){
-
-			relObj.scores.forEach(function(score){
-
-			    geneObj.push({
-				gene: linkingGene,
-				value: score.correlation_coefficient
-			    });
-			});
-		    });
-		}
-	    });
-	    dataObj.push({data: geneObj, name: 'Co-Expression'});
-
-	    // Set window for the matrix
-	    var margins = {top: 60, left: 80, right: 24, bottom: 24},
-		legendPanel = {
-		    width: 190
-		},
-		width = 800,
-		height = 1000 - margins.top - margins.bottom,
-		dataset = dataObj,
-		series = dataset.map(function (d) {
-		    return d.name;
-		}),
-		dataset = dataset.map(function (d) {
-		    return d.data.map(function (o, i) {
-			// Structure it so that your numeric
-			// axis (the stacked amount) is y
-			return {
-			    y: o.value,
-			    x: o.gene
-			};
-		    });
-		}),
-		stack = d3.layout.stack();
-	    stack(dataset);
-
-	    var dataset = dataset.map(function (group) {
-		return group.map(function (d) {
-		    // Invert the x and y values, and y0 becomes x0
-		    return {
-			x: d.y,
-			y: d.x,
-			x0: d.y0
-		    };
-		});
-	    }),
-		svg = d3.select('#matrix_container_d')
-		.append('svg')
-		.attr('width', width + margins.left + margins.right + legendPanel.width)
-		.attr('height', height + margins.top + margins.bottom)
-		.append('g')
-		.attr('transform', 'translate(' + margins.left + ',' + margins.top + ')'),
-		xMax = d3.max(dataset, function (group) {
-		    return d3.max(group, function (d) {
-			return d.x + d.x0;
-		    });
-		}),
-		xScale = d3.scale.linear()
-		.domain([0, xMax])
-		.range([0, width]),
-		months = dataset[0].map(function (d) {
-		    return d.y;
-		}),
-		_ = console.log(months),
-		yScale = d3.scale.ordinal()
-		.domain(months)
-		.rangeRoundBands([0, height], 0.1),
-		xAxis = d3.svg.axis()
-		.scale(xScale)
-		.orient('bottom'),
-		yAxis = d3.svg.axis()
-		.scale(yScale)
-		.orient('left'),
-		colours = d3.scale.category10(),
-		groups = svg.selectAll('g')
-		.data(dataset)
-		.enter()
-		.append('g')
-		.style('fill', function (d, i) {
-		    return colours(i);
-		}),
-		rects = groups.selectAll('rect')
-		.data(function (d) {
-		    return d;
-		})
-		.enter()
-		.append('rect')
-		.attr('x', function (d) {
-		    return xScale(d.x0);
-		})
-		.attr('y', function (d, i) {
-		    return yScale(d.y);
-		})
-		.attr('height', function (d) {
-		    return yScale.rangeBand();
-		})
-		.attr('width', function (d) {
-		    return xScale(d.x);
-		})
-		.on('mouseover', function (d) {
-		    var xPos = parseFloat(d3.select(this).attr('x')) / 2 + width / 2;
-		    var yPos = parseFloat(d3.select(this).attr('y')) + yScale.rangeBand() / 2;
-
-		    d3.select('#tooltip')
-			.style('left', xPos + 'px')
-			.style('top', yPos + 'px')
-			.select('#value')
-			.text(d.x);
-
-		    d3.select('#tooltip').classed('hidden', false);
-		})
-		.on('mouseout', function () {
-		    d3.select('#tooltip').classed('hidden', true);
-		});
-
-	    svg.append('g')
-		.attr('class', 'axis')
-		.attr('transform', 'translate(0,' + height + ')')
-		.call(xAxis);
-
-	    svg.append('g')
-		.attr('class', 'axis')
-		.call(yAxis);
-
-	    svg.append('rect')
-		.attr('fill', 'yellow')
-		.attr('width', 160)
-		.attr('height', 30 * dataset.length)
-		.attr('x', width + margins.left)
-		.attr('y', 0);
-
-	    series.forEach(function (s, i) {
-		svg.append('text')
-		    .attr('fill', 'black')
-		    .attr('x', width + margins.left + 8)
-		    .attr('y', i * 24 + 24)
-		    .text(s);
-		svg.append('rect')
-		    .attr('fill', colours(i))
-		    .attr('width', 60)
-		    .attr('height', 20)
-		    .attr('x', width + margins.left + 90)
-		    .attr('y', i * 24 + 6);
-	    });
-	}
-
 
 	/* - - - - - - - - - -  - -  - - - - - --*/
 	/* Generate a matrix wrapper  - - - - - -*/
@@ -459,13 +268,6 @@
 
 	    // The search is done, hide the waiting bar, clear error message container
 	    $(errorId).empty();
-
-	    // Set window for the matrix
-	    margin = {top: 60, right: 20, bottom: 10, left: 60}, width = 800, height = 800;
-
-	    x = d3.scale.ordinal().rangeBands([0, width]),
-	    z = d3.scale.linear().domain([0, 4]).clamp(true),
-	    c = d3.scale.category10().domain(d3.range(10));
 
 	    // Setup the matrix
 	    var im = initializeMatrix(json);
@@ -538,40 +340,20 @@
 
 	}
 
-	/* - - - - - - - - - -  - -  - - - - - --*/
-	/* Create a default graph - - - - - - - -*/
-	/* - - - - - - - - - -  - - - - - - - - -*/
-	function drawDefaultGeneGraph(){
-	    $('#wait_region4', appContext).html('<div id="loader_icon"><img src="https://apps.araport.org/jbrowse/plugins/EnsemblVariants/img/ajax-loader.gif"></div>');
-	    createGeneChartWrapper(default_gene_json);
-	}
-
-	// Define variables for matrix
-	var width, height, nodes, n, orders, timeout, type;
-	var matrix = [];
-	var margin = {top: 60, right: 20, bottom: 10, left: 60}, width = 800, height = 800;
-	var params = [];
-	var x = d3.scale.ordinal().rangeBands([0, width]),
-	    z = d3.scale.linear().domain([0, 4]).clamp(true),
-	    c = d3.scale.category10().domain(d3.range(10));
-
 	// Default display of groups
 	var filenameA = 'c5';
 	var filenameB = 'sc19';
 	var filenameC = 'kp04626';
-	var filenameD = 'AT5G63960';
 
 	// Default filter value
 	var filterValA = 0.5;
 	var filterValB = 0.5;
 	var filterValC = 0.5;
-	var filterValD = 0.5;
 
 	// Define clustering types
 	var errorA = '#errorA';
 	var errorB = '#errorB';
 	var errorC = '#errorC';
-	var errorD = '#errorD';
 
 	// Define clustering types
 	var orderA = '#orderA';
